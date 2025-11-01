070e31c2478155eca14f660ad92dabf04279ce71
lewis-tran <lewis.tran@linecorp.com>
2025-11-01 18:11:06 +0700

    Implement PhoneNumberSearchBar

---
+++ app/src/main/java/vn/dainghia/callinspector/ui/screen/home/search/composable/PhoneNumberSearchBar.kt
@@ -0,0 +1,75 @@
+package vn.dainghia.callinspector.ui.screen.home.search.composable
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PhoneNumberSearchBar(
+    onSearch: (String) -> Unit,
+    onFocusChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val textFieldState = rememberTextFieldState()
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused = interactionSource.collectIsFocusedAsState().value
+
+    LaunchedEffect(isFocused) {
+        onFocusChanged(isFocused)
+    }
+
+    BasicTextField(
+        state = textFieldState,
+        modifier = modifier,
+        interactionSource = interactionSource,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        onKeyboardAction = { onSearch(textFieldState.text.toString()) },
+        lineLimits = TextFieldLineLimits.SingleLine,
+        decorator = { innerTextField ->
+            TextFieldDefaults.DecorationBox(
+                value = textFieldState.text.toString(),
+                innerTextField = innerTextField,
+                enabled = true,
+                singleLine = true,
+                visualTransformation = VisualTransformation.None,
+                interactionSource = interactionSource,
+                leadingIcon = { Icon(Icons.Default.Search, null) },
+                shape = SearchBarDefaults.inputFieldShape,
+                label = if (textFieldState.text.isEmpty() && !isFocused) {
+                    { Text("Search phone number") }
+                } else {
+                    null
+                },
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                ),
+            )
+        }
+    )
+}
+
+@Preview
+@Composable
+fun SearchBarPreview() {
+    PhoneNumberSearchBar({}, {})
+}
