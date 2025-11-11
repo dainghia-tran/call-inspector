e86207c292b1e8e9ccb9bf43e0ae83281732e46b
lewis-tran <lewis.tran@linecorp.com>
2025-11-11 21:07:48 +0700

    Check and update config when app launch

--- app/src/main/java/vn/dainghia/callinspector/MainActivity.kt
+++ app/src/main/java/vn/dainghia/callinspector/MainActivity.kt
@@ -4,44 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import vn.dainghia.callinspector.ui.screen.home.HomeScreen
+import vn.dainghia.callinspector.ui.screen.home.settings.SettingsViewModel
+import vn.dainghia.callinspector.ui.screen.home.settings.isEligibleForCallerInfoOverlay
 import vn.dainghia.callinspector.ui.theme.CallInspectorTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val settingsViewModel: SettingsViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        checkAndUpdateConfig()
+
         setContent {
             CallInspectorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CallInspectorTheme {
-        Greeting("Android")
+    private fun checkAndUpdateConfig() {
+        val isEligible = isEligibleForCallerInfoOverlay(this)
+        if (!isEligible) {
+            settingsViewModel.updateShouldShowCallerInfoOverlay(false)
+        }
     }
 }
