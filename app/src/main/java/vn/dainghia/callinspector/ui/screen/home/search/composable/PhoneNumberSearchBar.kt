cf9e8d12c75b6601753115ff125e59eba718089c
lewis-tran <lewis.tran@linecorp.com>
2025-11-13 20:08:26 +0700

    Update color and keyboard type of TextField

--- app/src/main/java/vn/dainghia/callinspector/ui/screen/home/search/composable/PhoneNumberSearchBar.kt
+++ app/src/main/java/vn/dainghia/callinspector/ui/screen/home/search/composable/PhoneNumberSearchBar.kt
@@ -1,5 +1,6 @@
 package vn.dainghia.callinspector.ui.screen.home.search.composable
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
@@ -9,6 +10,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
@@ -16,11 +19,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.res.stringResource
 import vn.dainghia.callinspector.R
+import vn.dainghia.callinspector.ui.theme.CallInspectorTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -36,7 +41,11 @@ fun PhoneNumberSearchBar(
         state = textFieldState,
         modifier = modifier,
         interactionSource = interactionSource,
-        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Search,
+            keyboardType = KeyboardType.Phone
+        ),
+        textStyle = LocalTextStyle.current.copy(color = MaterialTheme.colorScheme.onSurface),
         onKeyboardAction = { onSearch(textFieldState.text.toString()) },
         lineLimits = TextFieldLineLimits.SingleLine,
         decorator = { innerTextField ->
@@ -64,11 +73,14 @@ fun PhoneNumberSearchBar(
 }
 
 @Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun SearchBarPreview() {
-    PhoneNumberSearchBar(
-        interactionSource = remember { MutableInteractionSource() },
-        isFocused = true,
-        onSearch = {}
-    )
+    CallInspectorTheme {
+        PhoneNumberSearchBar(
+            interactionSource = remember { MutableInteractionSource() },
+            isFocused = true,
+            onSearch = {}
+        )
+    }
 }
